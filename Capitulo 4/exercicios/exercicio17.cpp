78968ba753571b63b3a513137022476c5ce408d3
HenriqueIII <paulogoncalves.m@gmail.com>
2020-11-17 10:56:01 +0000

    some changes to exercicio 17
    added adivinha_i() - a iterative function that does the same as the recursive function adivinha(int bit)
    -------------
    raised the max number to 255
    -------------
    added some layout improvements in function lista(int bit)(it presents the list numbers in a convenient 16 column table)

--- Capitulo 4/exercicios/exercicio17.cpp
+++ Capitulo 4/exercicios/exercicio17.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <ctype.h>
+#include <iomanip>
 
-const int MAX=15;
+const int MAX=255;
 
 int respSim(){
     char resp;
@@ -11,15 +12,37 @@ int respSim(){
 }
 
 void lista(int bit){
-    for (int n=0;n <=MAX; ++n)
-        if (n & bit)
-            std::cout << ' ' << n;
+    int i=0;
+    for (int n=0;n <=MAX; ++n){
+        if (n & bit){
+            std::cout << std::setw(4) << n;
+            i++;
+        }
+        if ((i+1%16)==0)
+            std::cout << std::endl;
+    }
+}
+
+int adivinha_i(){
+    int bit = 1;
+    int num = 0;
+    for(int bit=1;bit<=MAX;bit=bit<<1){
+        lista(bit);
+        std::cout << "\nbit: " << bit << std::endl;
+        std::cout << "\n O Numero pertence a esta lista";
+        int resp=respSim();
+        if(resp){
+            num += bit;
+        }
+    }
+    return num;
 }
 
 int adivinha(int bit){
     if (bit >= MAX)
         return 0;
     lista (bit);
+    std::cout << "\nbit: " << bit << std::endl;
     std::cout << "\n O Numero pertence a esta lista";
     int resp = respSim();
     return (resp ? bit : 0) | (adivinha(bit<<1));
@@ -29,7 +52,7 @@ int main(){
     std::cout << "Pense num valor no intervalo [0.." <<MAX<<"]\n";
     std::cout << "e prima ENTER" << std::endl;
     while(std::cin.get() != '\n');
-    int res=adivinha(1);
+    int res=adivinha_i();
     std::cout << "O numero que pensou foi " << res << std::endl;
     return 0;
 }
