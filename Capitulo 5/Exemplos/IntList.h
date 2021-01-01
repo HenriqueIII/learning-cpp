37866ddffbfa0a82375604d2ecc51b7543dbe701
Paulo Gonçalves <paulogoncalves.m@gmail.com>
2021-01-01 23:34:48 +0000

    Created New Class IntList
    First decalration on class IntList

---
+++ Capitulo 5/Exemplos/IntList.h
@@ -0,0 +1,38 @@
+class IntList{
+
+    struct IntNode{                 //Classe interna para os nós da lista
+        int data;
+        IntNode *ptNext;
+        IntNode( int n ){data=n;}   //Construtor com int n.
+    };
+
+    IntNode * ptList; // Apontador para o primeiro nó
+
+    bool find( int val, IntNode * &prev, IntNode * &next) const;
+    //Inviabilizar a construção por cópia e a afectação
+    IntList(const IntList&){}
+    void operator=(const IntList&){}
+
+public:
+    IntList(){ ptList=NULL; }   //Iniciar a lista
+    ~IntList(){ removeAll();}   //Destruir todos os nós
+    bool insertNode(int val);   //Inserir ordenado
+    bool removeNode(int val);   //Remover se existir
+    int removeHead();           //Remover o 1º nó da lista
+    bool contain(int val) const;//Testar se existe val.
+    void display() const;       //Apresenta os elementos
+    bool isEmpty() const{       //Testar se a lista esta vazia
+        return ptList==NULL
+    }
+    void removeAll();           //Remover todos os elementos
+};
+
+bool IntList::contain(int val) const{
+    IntNode *pt = ptList;       //Apontador para o inicio
+    //Enquanto nao chegar ao fim e o corrente for menor
+    while(pt && pt -> data < val)
+        pt = pt -> ptNext;        //Avança para o sucessor
+    return pt && pt -> data == val;
+}
+
+
